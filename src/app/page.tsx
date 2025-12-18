6c67e429c089c2dfce3b10895cd979184a4ae55f
AIFuzi <eduard.li2017.ooo@yandex.ru>
2025-12-19 01:20:19 +0300

    feat(form): test 2FA field

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,31 +8,39 @@ import { Input } from '@/components/ui/input'
 export default function Home() {
   const [email, setEmail] = useState('')
   const [password, setPassword] = useState('')
+  const [totp, setTotp] = useState('')
 
   async function sendLogin() {
-    await LoginService.login(email, password)
+    await LoginService.login(email, password, totp)
   }
 
   return (
-    <div className="m-5 w-1/2 space-y-4">
-      <h1 className="text-3xl font-semibold">TestLogin</h1>
-      <Input
-        placeholder="Email"
-        autoComplete="off"
-        onChange={e => setEmail(e.target.value)}
-      />
-      <Input
-        placeholder="********"
-        autoComplete="off"
-        type="password"
-        onChange={e => setPassword(e.target.value)}
-      />
-      <Button
-        className="w-full"
-        onClick={() => sendLogin()}
-      >
-        Login
-      </Button>
+    <div className="flex h-screen items-center justify-center">
+      <div className="w-1/2 space-y-4">
+        <h1 className="text-3xl font-semibold">TestLogin</h1>
+        <Input
+          placeholder="Email"
+          autoComplete="off"
+          onChange={e => setEmail(e.target.value)}
+        />
+        <Input
+          placeholder="********"
+          autoComplete="off"
+          type="password"
+          onChange={e => setPassword(e.target.value)}
+        />
+        <Input
+          placeholder="2FA"
+          type="number"
+          onChange={e => setTotp(e.target.value)}
+        />
+        <Button
+          className="w-full"
+          onClick={() => sendLogin()}
+        >
+          Login
+        </Button>
+      </div>
     </div>
   )
 }
--- src/service/auth/login.service.ts
+++ src/service/auth/login.service.ts
@@ -1,7 +1,7 @@
 import { api } from '@/api/api'
 
 export class LoginService {
-  static async login(email: string, password: string) {
-    return await api.post('/session/login', { email, password })
+  static async login(email: string, password: string, pin: string) {
+    return await api.post('/session/login', { email, password, pin })
   }
 }
